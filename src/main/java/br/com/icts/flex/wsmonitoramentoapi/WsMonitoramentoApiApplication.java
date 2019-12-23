ef094be121536429496d9c5678de1c5f612d8325
eliasjr <elias.dvlpr@gmail.com>
2019-12-23 14:19:46 -0400

    start project

---
+++ src/main/java/br/com/icts/flex/wsmonitoramentoapi/WsMonitoramentoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.icts.flex.wsmonitoramentoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsMonitoramentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsMonitoramentoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/icts/flex/wsmonitoramentoapi/WsMonitoramentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.icts.flex.wsmonitoramentoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsMonitoramentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
